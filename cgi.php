426045e7752e570144323b6da49bed17c4b239fe
weaponofchoice -bruce- <broezer@gmail.com>
2013-09-27 11:09:27 +0200

    JS fix

--- cgi.php
+++ cgi.php
@@ -96,6 +96,17 @@
 		
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
+		$(document).ready(function(){
+			$('#resizeDiv')
+			    //.draggable()
+			    .resizable(
+					{
+					    handles: 'e, w'
+					});
+		})
+			
+		
+			
             (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
             function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
             e=o.createElement(i);r=o.getElementsByTagName(i)[0];
