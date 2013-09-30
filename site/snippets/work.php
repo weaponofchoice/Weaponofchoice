f6392bdb30115ee06f1189eb7623bab18ff58a1f
weaponofchoice -bruce- <broezer@gmail.com>
2013-09-30 15:48:08 +0200

    Work - Icons

--- site/snippets/work.php
+++ site/snippets/work.php
@@ -1,8 +1,8 @@
 
 <section id="work" class="fullscreen"  >
-	<div class="work-nav">
-	    <span id="prevControl">Prev </span>
-	    <span id="nextControl"> Next</span>
+	<div class="work-nav icon">
+	    <span id="prevControl">p </span>
+	    <span id="nextControl">n</span>
 	</div>	
 	
 	<div class="cycle-slideshow" data-cycle-fx="scrollHorz" data-cycle-slides="> div" data-cycle-timeout="0" data-cycle-prev="#prevControl" data-cycle-next="#nextControl" height="100%">
@@ -10,9 +10,9 @@
 		   <?php foreach($data->children()->visible() as $work): ?>
   			   <?php if($work->hero() !='' && $work->hero() !='0'): ?>  		
   			 	 <?php $image = $work->images()->find( html($work->hero()) ) ?>
-				 <a class="case-link" href="<?php echo $work->url() ?>" >more</a>
+				 
 			   	<div class="case">
-					
+					<a class="case-link icon" href="<?php echo $work->url() ?>" >+</a>
 			   		<div class="case-container container_3 " style="background-image:url('<?php echo $image->url() ?>');background-position: center; background-repeat: no-repeat; background-size: cover; -webkit-background-size: cover;
 				-moz-background-size: cover;
 				-o-background-size: cover;">
